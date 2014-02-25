2a4fea4536674d8cbcd23f0631f291f64867b396
jfields <fieldsjustin@gmail.com>
2014-02-25 10:44:17 -0500

    Remove unnecessary method.

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -1,15 +1,6 @@
 Spree::ProductsController.class_eval do
-  def index
-    params.merge!({'site_id' => current_tenant.site_id})
-    @searcher = build_searcher(params)
-    @products = @searcher.retrieve_products
-    respond_to do |format|
-      format.html
-      format.json { render :json => @products.map { |p| p.name } }
-    end
-  end
-
   def autocomplete
+    suggestions = Spree::Product.autosuggest(params[:keywords])
     render :text => suggestions.results.collect{|suggestion| { :id => suggestion.id, :label => suggestion.name, :value => suggestion.name} }.to_json
   end
 
