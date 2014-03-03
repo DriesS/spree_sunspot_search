e0c982723074427d95b294fcd7184a3e071d5e83
jfields <fieldsjustin@gmail.com>
2014-03-03 13:36:41 -0500

    Add site id to params before passing to autocomplete.

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -1,4 +1,6 @@
 Spree::ProductsController.class_eval do
+  before_action :merge_site_id, [:index, :autocomplete, :autosuggest]
+
   def autocomplete
     suggestions = Spree::Product.autocomplete(params[:keywords], params[:site_id])
     render :text => suggestions.results.collect{|suggestion| { :id => suggestion.id, :label => suggestion.name, :value => suggestion.name} }.to_json
@@ -7,5 +9,10 @@ Spree::ProductsController.class_eval do
   def autosuggest
     suggestions = Spree::Product.autocomplete(params[:keywords], params[:site_id])
     render :text => suggestions.results.collect{|suggestion| { :id => suggestion.id, :label => suggestion.name, :value => suggestion.name} }.to_json
+  end
+
+  private
+  def merge_site_id
+    params.merge!({'site_id' => spree_tenant.site_id})
   end
 end
