58b39876f8596d48d314c7fbef375b14423d7338
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2016-12-12 14:34:04 +0100

    add search class

--- app/overrides/add_search_facets.rb
+++ app/overrides/add_search_facets.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/shared/_taxonomies",
                       :name => "show_search_partials_facets",
                       :insert_top => "nav#taxonomies",
                       :partial => "spree/products/facets",
-                      :disabled => false)
+                      :disabled => false,
+                      :attributes => {:class => 'search-sidebar'}))
